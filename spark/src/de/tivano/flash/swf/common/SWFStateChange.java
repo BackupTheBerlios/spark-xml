2f867695d87c9cd08e5515bd5025a2776824d5e5
richard <richard>
2002-05-22 17:11:17 +0000

    Changed "Flash2XML" to "Spark" everywhere

--- spark/src/de/tivano/flash/swf/common/SWFStateChange.java
+++ spark/src/de/tivano/flash/swf/common/SWFStateChange.java
@@ -1,8 +1,8 @@
 /**
- * The contents of this file are subject to the Flash2XML Public
+ * The contents of this file are subject to the Spark Public
  * License Version 1.0 (the "License"); you may not use this file
  * except in compliance with the License. You may obtain a copy of the
- * License on the Flash2XML web site
+ * License on the Spark web site
  * (http://www.tivano.de/opensource/flash). 
  *
  * Software distributed under the License is distributed on an "AS IS"
@@ -10,14 +10,14 @@
  * the License for the specific terms governing rights and limitations
  * under the License. 
  *
- * The Initial Developer of Flash2XML is Tivano Software GmbH. The
- * original Flash2XML and portions created by Tivano Software GmbH are
+ * The Initial Developer of Spark is Tivano Software GmbH. The
+ * original Spark and portions created by Tivano Software GmbH are
  * Copyright Tivano Software GmbH. All Rights Reserved. 
  *
  * Contributor(s):
  *      Richard Kunze, Tivano Software GmbH.
  *
- * $Id: SWFStateChange.java,v 1.4 2002/05/21 08:39:59 kunze Exp $
+ * $Id: SWFStateChange.java,v 1.5 2002/05/22 17:11:17 richard Exp $
  */
 
 package de.tivano.flash.swf.common;
